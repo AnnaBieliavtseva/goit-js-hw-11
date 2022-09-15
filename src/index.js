5279a952669aab8d871b4558eda4132d795144dc
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2022-09-15 22:47:27 +0200

    add styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import Notiflix from 'notiflix';
+
 const axios = require('axios');
 
 const refs = {
@@ -11,37 +13,63 @@ const refs = {
 };
 
 refs.searchForm.addEventListener('submit', onSearch);
+refs.gallery.addEventListener('click', onGalleryRefClick);
+
+function onGalleryRefClick(event) {
+  event.preventDefault();
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
 
 async function onSearch(event) {
   event.preventDefault();
-  myGallery.searchQuery = event.currentTarget.elements.searchQuery.value;
+  myGallery.searchQuery = event.target.elements.searchQuery.value;
 
-  const backend = await myGallery.fetchPhotos();
-  onRenderGallery(backend.data.hits);
+  const backendData = await myGallery.fetchPhotos();
+
+  onRenderGallery(backendData.data.hits);
 }
 
 function onRenderGallery(photos) {
-  refs.gallery.innerHTML = photos
-    .map(
-      ({ largeImageURL, webformatURL, tags }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  refs.gallery.insertAdjacentHTML(
+    'beforeend',
+    photos
+      .map(
+        ({
+          largeImageURL,
+          webformatURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<div class="photo-card"><a href="${largeImageURL}" class="link">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" height="250" width="350"/></a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
+      <p>${likes}</p>
     </p>
     <p class="info-item">
       <b>Views</b>
+       <p>${views}</p>
     </p>
     <p class="info-item">
       <b>Comments</b>
+      <p>${comments}</p>
     </p>
     <p class="info-item">
       <b>Downloads</b>
+         <p>${downloads}</p>
     </p>
   </div>
 </div>`
-    )
-    .join('');
+      )
+      .join('')
+  );
 }
 
 class Gallery {
@@ -51,13 +79,34 @@ class Gallery {
   }
 
   async fetchPhotos(photo) {
-    const response = await axios.get(
-      `https://pixabay.com/api/?key=29882819-d1b2e59da7ad20757f8559035&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}`
-    );
-    return response;
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=29882819-d1b2e59da7ad20757f8559035&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+      );
+      this.incrementPage();
+      return response;
+    } catch (error) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      console.log(error.message);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
 const myGallery = new Gallery();
-let gallery = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
