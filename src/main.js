167ca49828152ee82cd34f5ffb0bbe0c44baf6a4
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-11-29 21:13:33 +0100

    add fetchApi and createMarkup functions

--- src/main.js
+++ src/main.js
@@ -1,28 +1,82 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
 
+const API_KEY = '29882819-d1b2e59da7ad20757f8559035';
+let userQuery;
 
-
-form.addEventListener('submit', onSearch);
-
-function onSearch(evt) {
-    evt.preventDefault();
-    const userQuery = evt.target.elements.searchQuery.value;
-    console.log(userQuery);
-}
-
-const API_KEY = '29882819 - d1b2e59da7ad20757f8559035';
-
-const params = {
+const params = new URLSearchParams({
   key: API_KEY,
-  q: `${userQuery}`,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
-};
+});
 
-console.log(params);
+function onSearch(evt) {
+  
+  evt.preventDefault();
+  userQuery = evt.target.elements.searchQuery.value.trim();
+  if (userQuery === "") {
+    return;
+  }
+  params.set('q', userQuery);
+  fetchApi();
+  console.log(params.toString());
+}
 
+form.addEventListener('submit', onSearch);
 
+function fetchApi() {
+  fetch(`https://pixabay.com/api/?${params}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(response => {
+      if (response.hits.length) {
+        gallery.insertAdjacentHTML('beforeend', createMarkup(response.hits));
+        console.log(response.hits);
+        
+      }
+      else {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+    })
+    .catch(error => 
+        console.log(error)
+      );
+}
 
-// fetch(`https://pixabay.com/api/?${params}`)
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `  <li  class="photo-card">
+          <a href="${largeImageURL} class="link">
+          <img src="${webformatURL}" alt="${tags}" width="300">
+          <div class="info">
+          <p>Likes: ${likes}</p>
+          <p>Views: ${views}</p>
+          <p>Comments: ${comments} </p>
+          <p>Downloads: ${downloads}</p>
+          </div>
+</a>
+        </li>`
+    )
+    .join('');
+}
